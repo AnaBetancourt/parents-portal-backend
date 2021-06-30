8662d3997d1dde1a3922a7e8f98a3b45e37195dc
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-06-29 20:35:01 -0400

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Meetup.create([
+    {title: "Watergun Fight!", date: "July 10th, 2021", time: 12, location: "Baker Park, Frederick, Maryland", description: "For ages 5-12! Bring your own water guns/balloons or use one of ours provided at the venue!", interested_count: 126},
+    {title: "Babies and Books", date: "May 1st, 2021", time: 9, location: "Cleveland Public Library, Cleveland, Ohio", description: "Bring your little ones for story time. Books focused on 0-18mo learning, siblings welcome.", interested_count: 54},
+    {title: "Paint Night", date: "November 9th, 2021", time: 7, location: "Joe's Pizza Place, Austin, Texas", description: "Family friendly paint night. Come and enjoy world-class pizza while creating some art!", interested_count: 35}
+])
+
+Post.create([
+    {body: 'What are the best activities for rainy days in with young kids?'},
+    {body: 'Someone, anyone, please.... tips for a dad just trying to navigate having a teenage daughter!?'},
+    {body: 'I found the greatest place to get the kids out for some nature fun!'}
+])
+
+Comment.create([
+    {body: 'Try making salt dough or putting paint into a large ziplock bag and letting them finger paint(mess free).', post_id: 1},
+    {body: 'Do not push!! Let her have some space and feel independent. Try to let her know you are there and support her.', post_id: 2},
+    {body: 'I have been dying to find a new place to go! Where is it?', post_id: 3}
+])
