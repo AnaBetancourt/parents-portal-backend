03c16517f55812f4a888416963d65ba0aab8cdd5
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-06-30 17:24:50 -0400

    created meetups controller

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,2 +1,35 @@
 class MeetupsController < ApplicationController
+
+    class CommentsController < ApplicationController
+
+        def index
+            meetups = Meetup.all
+            render json: MeetupSerializer.new(meetups)
+        end
+    
+        def create
+            meetup = Meetup.new(meetup_params)
+    
+            if meetup.save
+                render json: MeetupSerializer.new(meetup)
+            else
+                render json: {error: "Unable to add meetup."}
+            end
+    
+        end
+    
+        def show
+            meetup = Meetup.find(params[:id])
+            render json: MeetupSerializer.new(meetup)
+        end
+    
+    
+        private
+    
+        def meetup_params
+            params.require(:meetup).permit(:title, :date, :time, :location, :description, :interested_count)
+        end
+    
+    end
+
 end
