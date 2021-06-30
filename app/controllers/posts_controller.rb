f6b28d7265178cd3a765971d869cc41140d8445e
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-06-30 17:28:10 -0400

    created posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+    
+    def index
+        posts = Post.all
+        render json: PostSerializer.new(mposts)
+    end
+
+    def create
+        post = Post.new(post_params)
+
+        if post.save
+            render json: PostSerializer.new(post)
+        else
+            render json: {error: "Unable to add post."}
+        end
+
+    end
+
+    def show
+        post = Post.find(params[:id])
+        render json: PostSerializer.new(post)
+    end
+
+
+    private
+
+    def post_params
+        params.require(:post).permit(:body)
+    end
+
 end
