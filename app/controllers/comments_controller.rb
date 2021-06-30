9631af5a52daf1421ae28071b727d61ec4a99673
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-06-30 17:22:06 -0400

    created comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+
+    def index
+        comments = Comment.all
+        render json: CommentSerializer.new(comments)
+    end
+
+    def create
+        comment = Comment.new(comment_params)
+
+        if comment.save
+            render json: CommentSerializer.new(comment)
+        else
+            render json: {error: "Unable to add comment."}
+        end
+
+    end
+
+    def show
+        comment = Comment.find(params[:id])
+        render json: CommentSerializer.new(comment)
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:body, :post_id)
+    end
+
 end
