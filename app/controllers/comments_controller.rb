fc96632487cf9f46209ea5c5ea975f5f9d50348d
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-07-02 23:36:09 -0400

    removed index and show methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,5 @@
 class CommentsController < ApplicationController
 
-    def index
-        comments = Comment.all
-        render json: CommentSerializer.new(comments)
-    end
-
     def create
         comment = Comment.new(comment_params)
 
@@ -14,11 +9,6 @@ class CommentsController < ApplicationController
             render json: {error: "Unable to add comment."}
         end
 
-    end
-
-    def show
-        comment = Comment.find(params[:id])
-        render json: CommentSerializer.new(comment)
     end
 
 
